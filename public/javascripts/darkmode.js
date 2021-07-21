d716bf9466ffcd802cdfd38be7e90cc6714478d5
Glitch (nidhish) <none>
2021-07-21 02:45:14 +0000

    🍽🎣 Updated with Glitch

---
+++ public/javascripts/darkmode.js
@@ -0,0 +1,23 @@
+            function light() {
+            document.getElementById("body").style.backgroundColor = "white"
+            document.getElementById("html").style.backgroundColor = "white"
+            document.getElementById("title").style.color = "rgb(50,53,59)"
+            document.getElementById("foot").style.color = "rgb(50,53,59)"
+            document.getElementById("h2").style.color = "rgb(50,53,59)"
+            document.getElementById("h1").style.color = "rgb(50,53,59)"
+            document.getElementById("email").style.color = "rgb(50,53,59)"
+            document.getElementById("myTopnav").style.backgroundColor = "white"
+            document.getElementsByTagName("a").style.color = "rgb(50,53,59)"
+            }
+            function dark() {
+            document.getElementById("body").style.backgroundColor = "rgb(50,53,59)"
+            document.getElementById("html").style.backgroundColor = "rgb(50,53,59)"
+            document.getElementById("title").style.color = "white";
+            document.getElementById("foot").style.color = "white"
+            document.getElementById("h2").style.color = "white"
+            document.getElementById("h1").style.color = "white"
+            document.getElementById("email").style.color = "white"
+            document.getElementById("myTopnav").style.backgroundColor = "#2C2F33"
+            document.getElementsByTagName("a").style.color = "white"
+            }
+
--- routes/redirects/disserver.js
+++ routes/redirects/disserver.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('https://twitch.tv/sorekingjames')
+  res.redirect("https://discord.gg/w3x8tqgdc7")
 });
 
 module.exports = router;
